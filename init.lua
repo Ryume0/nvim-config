dace50a0c76b069de156c16ba3cdc91a7dfe4aaa
Ryume0 <pawzlo661@gmail.com>
2025-09-21 00:51:58 +0200

    ffs finally works

--- init.lua
+++ init.lua
@@ -16,15 +16,15 @@ vim.schedule(function()
 end)
 
 -- Enable break indent
---vim.opt.breakindent = true
+vim.opt.breakindent = true
 
 -- Save undo history
 vim.opt.undofile = true
 
 -- Indent setup
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
--- lua/plugins/basics.lua
+++ lua/plugins/basics.lua
@@ -1,10 +1,22 @@
 return {
-  {
-    'windwp/nvim-autopairs',
-    event = 'InsertEnter',
-    opts = {},
-  },
-  {
-    'NMAC427/guess-indent.nvim',
-  },
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		opts = {},
+	},
+	{
+		"NMAC427/guess-indent.nvim",
+	},
+	{ -- Adds git related signs to the gutter, as well as utilities for managing changes
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+		},
+	},
 }
